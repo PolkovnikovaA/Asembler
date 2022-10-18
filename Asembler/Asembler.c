d6a13894e4d9bcb030eea79afc733e788c40c8e3
PolkovnikovaA <21nastya.chernaya19@gmail.com>
2022-10-18 11:16:34 +0300

    Final

--- Asembler/Asembler.c
+++ Asembler/Asembler.c
@@ -1,168 +1,285 @@
 ﻿#include<stdio.h>
 #include <time.h>
-int main(int argc, char* argv[]) {
-    system("chcp 1251>nul");
+#include <locale.h>
 
 
-    int a = 5;
-    int b = 15;
- /*   int c = plus(a,b);   
-    printf("%d + %d = %d\n", a, b, c);
+    int main()
+    {
 
+        setlocale(LC_ALL, "RUS");
+        for (int i = 0; i < 10000; i++) 
+        {
+            ymn(-5, -2);
+        }
+        
+        //sum(2, 5);
+        //razn(5, 2);
+        //ymn(-5, -2);
+        //del(10, -5);
+        //del_oct(20, 7);
+       //crav(1, 5);
+        //crav_3(4, 2, 4);
+        //swap(5, 1);
+        //sumc(2, 5);
+        //razmc(5, 2);
+        //delc(10, -5);
+        //delos(20, 7);
+        //sravc(1, 5);
+        //crav_C(4,1,4);
+        //swap_c(5, 1);
+        printf("\n%d", clock());
+    }
 
-    int d = minus(a, b);
-    printf("%d - %d = %d\n", a, b, d);
+    int swap_c(int a, int b) 
+    {
+        int a1 = a;
+        int b1 = b;
+        b = a1;
+        a = b1;
+      /*  printf("%d %d", a, b);*/
+    }
 
-    int e = division(a, b);
-    printf("%d / %d = %d\n", a, b, e);
-
-
-    int f = multiplication(a, b);
-    printf("%d * %d = %d\n", a, b, f);
-
-    int g = divisionost(a, b);
-    printf("%d / %d = %d\n", a, b, g);*/
+    int sravc(int a, int b)
+    {
+           if (a>b)
+           {
+               printf("1");
+           }
+           else if (a < b) 
+           {
+               printf("2");
+           }
+           else 
+           {
+               printf("0");
+           }
     
+    }
 
-   /* int h = plus1(a, b);
-    printf("%d + %d = %d\n", a, b, h);*/
+    int crav_C(int a, int b, int c) 
+    {
+        if (a > b && a>c)
+        {
+            printf("1");
+        }
+        else if (a < b && b>c)
+        {
+            printf("2");
+        }
+        else if (c > b && c > a)
+        {
+            printf("3");
+        }
+        else if (a == b && a > c)
+        {
+            printf("1 и 2 равны");
+        }
+        else if (c == b && a < c)
+        {
+            printf("2 и 3 равны");
+        }
+        else if (c == a && a > b)
+        {
+            printf("1 и 3 равны");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
 
-    /*int i = minus1(a, b);
-    printf("%d + %d = %d\n", a, b, i);*/
+    int delos(int a, int b) 
+    {
+        return a % b;
+    }
+
+    int delc(int a, int b) 
+    {
+        return a / b;
+    }
+
+    int ymnc(int a, int b) 
+    {
+        return a * b;            
+    }
+
+    int sumc(int a, int b)
+    {
+        return a + b;
+    }
+
+    int razmc(int a, int b)
+    {
+        return a - b;    
+    }
+
+    int sum(int a, int b)
+    {
+        int otv;
+        __asm
+        {
+            mov eax, a
+            mov ebx, b
+            add eax, ebx
+            mov otv, eax
+        }
+        /*printf("Сумма: %d\n", otv);*/
+    }
+
+    int razn(int a, int b)
+    {
+        int otv;
+        __asm
+        {
+            mov eax, a
+            mov ebx, b
+            sub eax, ebx
+            mov otv, eax
+        }
+      /*  printf("Разность: %d\n", otv);*/
+    }
+
+    int ymn(int a, int b)
+    {
+        int otv;
+        __asm
+        {
+            mov eax, a
+            mov ebx, b
+            imul ebx
+            mov otv, eax
+        }
+      /*  printf("Умножение: %d\n", otv);*/
+    }
+
+    int del(int a, int b)
+    {
+        int otv;
+        __asm
+        {
+            mov eax, a
+            mov ebx, b
+            mov edx, 0
+            idiv ebx
+            mov otv, eax
+        }
+        /*printf("Деление: %d\n", otv);*/
+    }
+
+    int del_oct(int a, int b)
+    {
+        int otv;
+        __asm
+        {
+            mov eax, a
+            mov ecx, b
+            mov edx, 0
+            div ecx
+            mov otv, edx //остаток от деления
+
+        }
+       /* printf("Остаток от деления: %d\n", otv);*/
+    }
+
+    int crav(int a, int b)
+    {
+        _asm
+        {
+            mov eax, a
+            mov ebx, b
+            cmp eax, ebx
+
+            je equal
+            jg greater
+            jmp less
+
+            equal :
+            mov a, 0
+                jmp exit
+
+                greater :
+            mov a, 1
+                jmp exit
+
+                less :
+            mov a, 2
+                jmp exit
+
+                exit :
+        }
+       /* printf("Сравнение 2 чисел: %d\n", a);*/
+    }
+
+    int crav_3(int a, int b, int c)
+    {
+        _asm
+        {
+            mov eax, a
+            mov ebx, b
+            mov ecx, c
+            cmp eax, ebx
+            je equal
+            jg greater
+            jmp less
 
 
-   /* int j = Del(a, b);
-    printf("%d / %d = %d\n", a, b, j);*/
+            equal :
+            cmp eax, ecx
+                je equal2
+                jg greater2
+                jmp less2
 
+                greater :
+            cmp eax, ecx
+                je equal2
+                jg greater2
+                jmp less2
 
-   /* int k = ymn(a, b);
-    printf("%d * %d = %d\n", a, b, k);*/
+                less :
+            cmp ebx, ecx
+                je equal2
+                jg greater3
+                jmp less2
 
+                equal2 :
 
-  /*  int l = del(a, b);
-    printf("%d / %d = %d\n", a, b, l);*/
+            mov a, 0
+                jmp exit
 
-    printf("%d", clock());
+                greater2 :
+            mov a, 1
+                jmp exit
+
+                less2 :
+            mov a, 3
+                jmp exit
+
+                greater3 :
+            mov a, 2
+                jmp exit
+
+                exit :
+        }
+       /* printf("Сравнение 3 чисел : %d\n", a);*/
+    }
 
     
-
-
-    return 0;
-
-}
-    
-
-
-int plus(int a, int b) 
-{
-    int c;
-    __asm 
+    int swap(int a, int b)
     {
-        mov    eax, a
-        mov    ebx, b
-        add    eax, ebx
-        mov    c, eax
+        int c;
+        __asm
+        {
+            mov eax, a
+            mov ebx, b
+
+            mov ecx, eax
+            mov eax, ebx
+            mov ebx, ecx
+
+            mov a, eax
+            mov b, ebx
+
+        }
+       /* printf("Замена : %d %d\n", a, b);*/
+
     }
-    return c;
-}
-int minus(int a, int b)
-{
-    int d;
-    __asm
-    {
-        mov    eax, a
-        mov    ebx, b
-        sub    eax, ebx
-        mov    d, eax
-    }
-    return d;
-}
-int division(int a, int b)
-{
-    int e;
-
-    __asm {
-        mov eax, a
-        mov ebx, b
-        xor edx, edx;
-        idiv ebx;
-        mov e, eax;
-    }
-    return e;
-}
-
-
-int multiplication(int a, int b)
-{
-    int f;
-
-    __asm {
-        mov eax, a
-        mov ebx, b
-        xor edx, edx;
-        imul ebx;
-        mov f, eax;
-    }
-    return f;
-}
-
-
-int divisionost(int a, int b)
-{
-    int g;
-
-    __asm {
-        mov eax, a
-        mov ebx, b
-        xor edx, edx;
-        idiv ebx;
-        mov g, edx;
-    }
-    return g;
-}
-
-
-int plus1(int a, int b) 
-{
-      return a+b;
-}
-
-int minus1(int a, int b)
-{
-    return a - b;
-}
-
-int Del(int a, int b)
-{
-    return a / b;
-}
-
-int ymn(int a, int b)
-{
-    return a * b;
-}
-
-int del(int a, int b)
-{
-    return a % b;
-}
-
-int srav(int a, int b)
-{
-    if (a > b) 
-    {
-        int c = 1;
-        return c;
-    }
-    else if (a < b)
-    {
-        int d = 2;
-        return d;
-    }
-    else 
-    {
-        int f = 0;
-        return f;
-    }
-
-}
